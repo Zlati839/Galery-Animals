b54411bfe184093b972ee1d5e42fce96327d2894
Zlati839 <zlatipenev7@gmail.com>
2026-03-24 14:27:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -43,6 +43,5 @@ if st.session_state.animals:
   cols = st.columns(3)
   for idx , animal in enumerate(st.session_state.animals):
     with cols[idx % 3]:
-      st.subheader(animal["name"])
       st.image(animal["photo"], use_column_width= True)
       st.write(animal["description"])
